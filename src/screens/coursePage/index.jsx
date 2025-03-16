2adeb6fcf2bb27b3f2df2ba7445e7d4ab6bd962d
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-03-16 10:06:16 +0330

    base

---
+++ src/screens/coursePage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Courses from '../../components/courses'
+
+const CoursesPage = () => {
+  return (
+    <div><Courses/></div>
+  )
+}
+
+export default CoursesPage
