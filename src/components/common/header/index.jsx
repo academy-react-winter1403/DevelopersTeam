1cf089b53807f67e1141b74da687c4f6ad55f86f
rezvan <rezvane@gmail.com>
2025-03-17 14:10:21 +0330

    2th day

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div  className='w-[1320px] h-[56px]  border-2 border-[#E4E4E4] mt-5 mx-auto'>
+
+    </div>
   )
 }
 
