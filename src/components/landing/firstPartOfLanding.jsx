9ced847b9660540ee20ac4e0af77898399bc1bf5
rezvan <rezvane@gmail.com>
2025-03-18 12:14:46 +0330

    git

---
+++ src/components/landing/firstPartOfLanding.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstPartOfLanding = () => {
+  return (
+    <div>
+      <h2>FirstPartOfLanding</h2>
+    </div>
+  )
+}
+
+export default FirstPartOfLanding
--- src/screens/landingPage/index.jsx
+++ src/screens/landingPage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Landing from '../../components/landing'
 import HeroSection from '../../components/landing/heroSection'
+import FirstPartOfLanding from '../../components/landing/firstPartOfLanding'
 
 const LandingPage = () => {
   return (
     <div>
       <HeroSection/>
+      <FirstPartOfLanding/>
     </div>
   )
 }
