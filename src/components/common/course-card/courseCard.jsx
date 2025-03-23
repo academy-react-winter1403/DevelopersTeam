5e7512ed8a7fe90f3d9ccc907b8cefd2b90e553c
rezvan <rezvane@gmail.com>
2025-03-23 10:58:34 +0330

    8th day

--- src/components/common/course-card/courseCard.jsx
+++ src/components/common/course-card/courseCard.jsx
@@ -5,39 +5,46 @@ import StudentIcon from "./../../../assets/images/students-stroke-rounded 1.svg"
 import ThumbUp from "./../../../assets/images/thumbs-up-stroke-rounded 1.svg";
 import thumbDown from "./../../../assets/images/thumb-down.svg";
 
-
-
-
-const CourseCard = ({title , img , teacherName , statusName , describe , student , cost , likeCount , dissLikeCount}) => {
+const CourseCard = ({ title, img, teacherName, statusName, describe, student, cost, likeCount, dissLikeCount }) => {
   return (
-    <div className="h-[400px] w-80 border-2 border-gray-50 rounded-2xl bg-gray-50">
-      <div className="h-1/3 rounded-3xl">
-        <img src={img} alt="" />
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-[400px] border-2 border-gray-50 rounded-2xl bg-gray-50 flex flex-col">
+      <div className="h-1/3">
+        <img src={img} alt="" className="w-full h-full object-cover rounded-t-2xl" />
       </div>
-      <div>
-        <h2 className="mt-14 mr-2">{title}</h2>
-      </div>
-      <div className="flex flex-wrap">
-        <h2 className="text-gray-400 text-[11px] mt-2 mr-2 overflow-hidden whitespace-nowrap text-ellipsis">{describe}</h2>
-      </div>
-      <div className="flex gap-2 mt-3 mr-2 leading-4 text-[12px]">
-        <img src={TeacherIcon} alt=""  className="h-5 w-5"/> {teacherName}
-      </div>
-      <div className="flex gap-2 mt-3 mr-2 leading-4 text-[12px]">
-        <img src={CalenderIcon} alt="" className="h-5 w-5"/> {statusName}
-      </div>
-      <div className="flex gap-2 mt-3 mr-2 leading-4 text-[12px]">
-        <img src={StudentIcon} alt="" className="h-5 w-5"/><span className="text-[14px] leading-6">{student}</span>
-      </div>
-      <div className="flex justify-around gap-4 mt-2">
+      <div className="p-4 flex flex-col flex-grow">
         <div>
-         <span className="text-[15px]">{cost}</span> <span className="text-blue-400 text-[15px]">تومان</span>
-        </div>       
-         <div className="flex gap-1">
-         <img src={ThumbUp} alt=""  className="w-6 h-6"/>{likeCount}
-         </div>
-        <div className="flex gap-1">
-          <img src={thumbDown} alt="" className="w-4 h-5"/> {dissLikeCount}
+          <h2 className="text-lg font-semibold text-gray- line-clamp-1">{title}</h2>
+          <p className="text-gray-400 text-sm mt-2 line-clamp-2">{describe}</p>
+        </div>
+        <div className="mt-auto">
+          <div className="flex items-center gap-2 mt-2 text-sm">
+            <img src={TeacherIcon} alt="" className="h-5 w-5" />
+            <span>{teacherName}</span>
+          </div>
+          <div className="flex items-center gap-2 mt-2 text-sm">
+            <img src={CalenderIcon} alt="" className="h-5 w-5" />
+            <span>{statusName}</span>
+          </div>
+          <div className="flex items-center gap-2 mt-2 text-sm">
+            <img src={StudentIcon} alt="" className="h-5 w-5" />
+            <span className="text-md">{student}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center p-4">
+        <div>
+          <span className="text-md">{cost}</span> <span className="text-blue-400 text-[15px]">تومان</span>
+        </div>
+        <div className="flex gap-2">
+          <div className="flex items-center gap-1">
+            <img src={ThumbUp} alt="" className="w-6 h-6" />
+            <span>{likeCount}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <img src={thumbDown} alt="" className="w-4 h-5" />
+            <span>{dissLikeCount}</span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/landing/firstPartOfLanding.jsx
+++ src/components/landing/firstPartOfLanding.jsx
@@ -7,8 +7,8 @@ import AxPanel from "./../../assets/images/AxPanel.png";
 const FirstPartOfLanding = () => {
   return (
     <>
-      <div className=" mt-16 mx-auto md:flex md:flex-row gap-4 justify-center border-2 md:w-10/12">
-        <div className="text-right indent-2  w-[300px] h-[45%] mx-auto sm:mx-0 mb-6 border-gray-300 border-2 rounded-2xl relative xl:w-4/12">
+      <div className=" mt-16 mx-auto md:flex md:flex-row gap-4 justify-center md:w-10/12">
+        <div className="text-right indent-2  w-[300px] h-[45%] mx-auto sm:mx-0 mb-6 border-gray-300 border-2 rounded-2xl relative xl:w-4/12 md:h-64">
           <h1 className="font-bold mt-4 text-[20px] ">
             پنل اختصاصی دانشجو
           </h1>
@@ -16,10 +16,10 @@ const FirstPartOfLanding = () => {
           <h6 className=" text-[12px]  mt-2 indent-4 ">
             برای مدیریت دوره ها و تمرین ها
           </h6>
-          <img src={AxPanel} alt="" className="w-[50%] h-[50%] mt-4 xl:mr-53  rounded-2xl" />
+          <img src={AxPanel} alt="" className="w-[50%] h-[50%] mt-4 xl:mr-48  rounded-2xl" />
         </div>
-        <div className="w-[300px] h-[45%] border-gray-300 border-2 rounded-2xl bg-[#3772FF] relative xl:w-3/12">
-          <h1 className="text-white font-bold mt-4 mr-3">
+        <div className="w-[300px] indent-2 mx-auto sm:mx-0 h-[45%] border-gray-300 border-2 rounded-2xl bg-[#3772FF] relative xl:w-3/12 md:h-64">
+          <h1 className="text-white font-bold mt-4">
             دوره های جدید تابستانه!
           </h1>
           <div className="w-36 h-16 mt-2 text-[13px] mr-3 text-white">
@@ -29,27 +29,27 @@ const FirstPartOfLanding = () => {
             <img
               src={arrowUpLeft}
               alt=""
-              className=" rounded-full bg-white "
+              className=" rounded-full bg-white absolute top-2 left-2 "
             />
           </Link>
           <img
             src={Group}
             alt=""
-            className="w-40 h-36 absolute left-0 top-18"
+            className="w-[50%] h-[40%] "
           />
         </div>
-        <div className="w-[270px] border-gray-300 border-2 rounded-2xl relative xl:w-2/12">
+        <div className="w-[300px] indent-2 mx-auto sm:mx-0 h-[45%] mt-4 md:h-64 md:mt-0 border-gray-300 border-2 rounded-2xl relative xl:w-2/12">
           <div>
             <h3 className="mt-3 mr-2">درباره ما</h3>
             <h4 className=" mr-2">بیشتر بخوانید</h4>
           </div>
-          <div className="mt-9 mr-2">
+          <div className="mt-15 mr-2">
             <h1>+1000</h1>
             <h6 className="text-[10px] text-gray-500">دانشجو آنلاین در دوره</h6>
           </div>
-          <div className="mt-5 mr-2">
+          <div className="mt-10 mr-2">
             <h1>+13</h1>
-            <h6 className="text-[10px] text-gray-500">سال سابقه آموزشی</h6>
+            <h6 className="text-[10px] text-gray-500 mb-4">سال سابقه آموزشی</h6>
           </div>
           <Link>
             <img
@@ -68,18 +68,22 @@ const FirstPartOfLanding = () => {
         <ul className="mt-1">خدماتی که ما به شما ارائه می دهیم</ul>
         <ul className="mt-1">خدماتی که ما به شما ارائه می دهیم</ul>   
       </div>
-      <div className="w-[1100px] h-48 mt-20 mx-auto flex flex-row gap-3">
-        <div className="w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
+      <div className=" mt-20 mx-auto flex flex-row gap-3 p-4 justify-center items-center flex-wrap ">
+        {/* card1*/}
+        <div className="w-full sm:w-1/2 md-1/4 max-w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
          <h1 className="border-b-2 border-gray-300 w-[230px] mt-4 pb-3 mx-auto text-[20px]">01</h1>
          <h3 className="mr-2 mt-2">مدرک معتبر</h3>
         </div>
-        <div className="w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
+        {/* card2*/}
+        <div className="w-full sm:w-1/2 md:w-1/4 max-w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
          <h1 className="border-b-2 border-gray-300 w-[230px] mt-4 pb-3 mx-auto text-[20px]">02</h1>
          <h3 className="mr-2 mt-2">مدرک معتبر</h3>
-        </div> <div className="w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
+        {/* card3*/}
+        </div> <div className="w-full sm:w-1/2 md:w-1/4 max-w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
          <h1 className="border-b-2 border-gray-300 w-[230px] mt-4 pb-3 mx-auto text-[20px]">03</h1>
          <h3 className="mr-2 mt-2">مدرک معتبر</h3>
-        </div> <div className="w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
+        {/* card4*/}
+        </div> <div className="w-full sm:w-1/2 md:w-1/4 max-w-[250px] border-gray-400 border-[1px] h-44 rounded-2xl">
          <h1 className="border-b-2 border-gray-300 w-[230px] mt-4 pb-3 mx-auto text-[20px]">04</h1>
          <h3 className="mr-2 mt-2">مدرک معتبر</h3>
         </div>
