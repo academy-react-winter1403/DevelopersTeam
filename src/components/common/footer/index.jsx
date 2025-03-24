702cd1e61ad0eb66881f8e2d1aced71ba4cf19ed
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-03-24 17:37:05 +0330

    footer conflict

--- src/components/common/footer/index.jsx
+++ src/components/common/footer/index.jsx
@@ -86,33 +86,6 @@ const Footer = () => {
           </div>
         
       </div>
-<<<<<<< HEAD
-      
-      <div className='w-3/12 h-60 mr-10'>
-        <h1 className='font-bold h-10 indent-5 mt-7 text-[20px]'>آکادمی کدنویسی بحر</h1>
-        <h5 className='text-[15px] text-gray-600 mt-2 w-72 mx-auto'>+13 سال سابقه فعالیت در زمینه آموزش کدنویسی از سنین کودکی تا بزرگسال. هدف ما همیشه این بوده که دانشجویان را با مهارت های لازم برای موفقیت در دنیای فناوری و برنامه نویسی مجهز کنیم.</h5>
-      </div>
-
-      <div className='w-2/12 h-60 mr-20'>
-        <h1 className='text-gray-600 leading-14 mt-5 mr-6'>صفحات</h1>
-        <Link><h2 className=' mr-6'>خانه</h2></Link>
-        <Link><h2 className=' mr-6'>دوره ها</h2></Link>
-        <Link><h2 className=' mr-6'>دوره و مقالات</h2></Link>
-      </div>
-
-      <div className='w-2/12 h-60'>
-      <h1 className='text-gray-600 leading-14 mt-5 mr-6'>ما</h1>
-        <Link><h2 className=' mr-6' >اساتید</h2></Link>
-        <Link><h2  className=' mr-6' >درباره ما</h2></Link>
-        <Link><h2  className=' mr-6'>ارتباط با ما </h2></Link>
-      </div>
-      <HugeiconsIcon icon={ArrowRight01Icon} />
-
-      <div className='w-4/12 h-60'>
-        <h2 className='w-24 h-14 bg-gray-300 mt-15 mr-28 pr-3'>نماد اعتماد الکترونیک</h2>
-      </div>
-=======
->>>>>>> fateme-landing
     </div>
   );
 };
