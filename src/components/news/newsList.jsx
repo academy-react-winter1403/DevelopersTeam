a7a22d40e6595f486c479c62435c33399906ee42
rezvan <rezvane@gmail.com>
2025-03-25 13:53:49 +0330

    10th day

--- src/components/news/newsList.jsx
+++ src/components/news/newsList.jsx
@@ -3,6 +3,7 @@ import http from '../../core/services/interceptor'
 import { useQuery } from '@tanstack/react-query';
 import CoursesNavbar from '../courses/coursesNavbar/coursesNavbar';
 import FilterPartOfNews from './filterPartOfNews';
+import NewsCard from '../common/news-card/newsCard';
 
 const NewsList = () => {
 
@@ -23,7 +24,19 @@ const NewsList = () => {
     <div className="grid grid-cols-4 h-auto m-4 border-4 border-borderGray rounded-4xl">
       <div className="col-span-4 lg:col-span-3 w-full ">
         <CoursesNavbar />
-        <div></div>
+        <div>
+          {data?.news.map((item,index)=>{
+            return (
+              <NewsCard
+                key={item.id}
+                addUserProfileImage={item.addUserProfileImage}
+                title={item.title}
+                miniDescribe={item.miniDescribe}
+                addUserFullName={item.addUserFullName}
+              />
+            )
+          })}
+        </div>
       </div>
       <div className="hidden md:block p-8">
         <FilterPartOfNews />
