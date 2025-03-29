d4e02ad6227ec0a03c22b3c207305aefb04467de
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-03-30 00:48:40 +0330

    .

--- src/components/courses/gridCourseCard/gridCourseCard.jsx
+++ src/components/courses/gridCourseCard/gridCourseCard.jsx
@@ -16,15 +16,14 @@ const GridCourseCard = ({
   student,
   cost,
   likeCount,
-  dissLikeCount,miniDescribe,addUserFullName
+  dissLikeCount,
+  miniDescribe,
+  addUserFullName,
 }) => {
   return (
-    <div
-      // className="w-3/5 sm:w-2/5 md:w-1/3 lg:w-1/4 xl:w-1/6 h-[370px] xl:h-[400px] border-2 border-[#E4E4E4] rounded-2xl bg-gray-50 flex flex-col"
-      className="w-full h-72 bg-lightGray border-2 border-red-400 grid grid-cols-5 overflow-hidden rounded-3xl mr-3"
-    >
+    <div className="w-full h-72 bg-lightGray border-2 grid grid-cols-5 overflow-hidden rounded-3xl mr-3">
       <div className="col-span-2 bg-red-400 border-2 rounded-3xl"></div>
-      
+
       <div className="col-span-3 border-2 border-green-400">
         <div>{title}</div>
         <div></div>
