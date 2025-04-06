daac61a1230045df8923cbff4d9bef716cacec37
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-06 15:27:01 +0330

    panel

--- src/components/panel/dashboard/mainDashboard/progress/progressComp.jsx
+++ src/components/panel/dashboard/mainDashboard/progress/progressComp.jsx
@@ -1,10 +1,22 @@
-import { Progress } from "antd";
 import React from "react";
+import Progress from "react-circle-progress-bar";
+import { LuPencilLine } from "react-icons/lu";
 
 const ProgressComp = () => {
   return (
-    <div className="col-span-2 border">
-      <Progress type="circle" percent={75} style={{width:"300px",border:'1px solid red'}} />
+    <div className="col-span-2 flex flex-col justify-c enter items-center bg-[#FEFDFF] rounded-2xl">
+      <div className="flex justify-between w-full px-5 mt-5">
+        <span className="font-semibold">وضعیت اطلاعات حساب</span>
+        <span>
+          <LuPencilLine className="w-5 h-5 text-navyBlue" />
+        </span>
+      </div>
+      <div className="mt-2">
+        <Progress progress={75} />
+      </div>
+      <div>
+        <h1 className="text-navyBlue">اطلاعات حساب‌کابری شما تکمیل است</h1>
+      </div>
     </div>
   );
 };
