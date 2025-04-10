abe40594480f46859e0a3c8c9d50b7b2782a06cf
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-10 14:35:58 +0330

    your address

--- src/components/panel/profile/userAddress/userAddress.jsx
+++ src/components/panel/profile/userAddress/userAddress.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const UserAddress = () => {
   return (
-    <div>UserAddress</div>
+    <div className='w-full mb-10'>
+      <h1 className='text-navyBlue'>داخل نقشه موقعیت مکانی محل سکونت خود را انتخاب کنید</h1>
+      <div className='bg-lightGray w- full h-96 ml-10 rounded-2xl mt-2'>
+
+      </div>
+    </div>
   )
 }
 
