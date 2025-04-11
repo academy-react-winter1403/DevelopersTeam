b0da1ee95f3703d0bbd4719cbc38bdb33012bf1e
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-12 00:14:42 +0330

    comment section

--- src/components/courseDetail/commentSection/provider/provider.jsx
+++ src/components/courseDetail/commentSection/provider/provider.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import http from "./../../../../core/services/interceptor";
 import { useQuery } from "@tanstack/react-query";
-import ReplyCard from "../replyCard/replyCard";
 import CommentCard from "../commentCard/commentCard";
 
 const Provider = ({ courseId, commentId }) => {
@@ -18,9 +17,21 @@ const Provider = ({ courseId, commentId }) => {
   });
 
   return (
-    <div>
+    <div className="mr-10">
+      <div className="w-2 h-auto bg-navyBlue rounded-sm mr-4"></div>
       {data?.map((item) => (
-        <ReplyCard data={item} />
+        <CommentCard
+          author={item?.author}
+          insertDate={item?.insertDate}
+          title={item?.title}
+          describe={item?.describe}
+          pictureAddress={item?.pictureAddress}
+          courseId={item?.courseId}
+          commentId={item?.id}
+          likeCount={item?.likeCount}
+          disslikeCount={item?.disslikeCount}
+          currentUserEmotion={item?.currentUserEmotion}
+        />
       ))}
     </div>
   );
