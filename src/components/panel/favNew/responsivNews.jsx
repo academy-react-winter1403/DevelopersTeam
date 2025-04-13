a5aa3323565783bbce5948c9c7d4aeb10f01cf0f
rezvan <rezvane@gmail.com>
2025-04-13 07:47:28 +0330

    26th day

---
+++ src/components/panel/favNew/responsivNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResponsivNews = ({ data }) => {
+  return (
+    <div className='border w-96 h-52 bg-amber-400'>
+      
+    </div>
+  )
+}
+
+export default ResponsivNews
--- src/components/panel/favNew/tableNews/tableFaveNews.jsx
+++ src/components/panel/favNew/tableNews/tableFaveNews.jsx
@@ -6,7 +6,7 @@ import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import BodyTableNews from "./bodyTableNews";
 import defImg from "./../../../../assets/images/courses/courseimg.svg";
-
+import ResponsivNews from "../responsivNews";
 
 const TableFaveNews = () => {
   const [convertedData, setCovertedData] = useState([]);
@@ -15,22 +15,28 @@ const TableFaveNews = () => {
     const res = await http.get(`/SharePanel/GetMyFavoriteNews`);
     return res;
   };
-  const icons = <div className="flex gap-5">
-    <MdOutlineRemoveRedEye className="w-6 h-6 text-gray" />
-    <IoMdClose className="w-6 h-6 text-red-500"/>
-  </div>
+  const icons = (
+    <div className="flex gap-5">
+      <MdOutlineRemoveRedEye className="w-6 h-6 text-gray" />
+      <IoMdClose className="w-6 h-6 text-red-500" />
+    </div>
+  );
 
   const { data, isSuccess } = useQuery({
     queryKey: "favNewsPanel",
     queryFn: getFavNews,
   });
 
-   const img = (
-      <img
-        src={data?.currentImageAddressTumb == null ? defImg : el.currentImageAddressTumb}
-        alt=""
-      />
-    );
+  const img = (
+    <img
+      src={
+        data?.currentImageAddressTumb == null
+          ? defImg
+          : el.currentImageAddressTumb
+      }
+      alt=""
+    />
+  );
 
   useEffect(() => {
     if (isSuccess) {
@@ -38,25 +44,32 @@ const TableFaveNews = () => {
         let newData = {};
         newData["img"] = img;
         newData["name"] = el.title;
-        newData["desc"] = 'آموزش صفر تا صد کتابخانه پرطرفدار جی‌اس یعنی ری‌اکت همراه تسک های مفید برای یادگیری بهتر';
-        newData["teacher"] = 'محسن اسفندیاری';
+        newData["desc"] =
+          "آموزش صفر تا صد کتابخانه پرطرفدار جی‌اس یعنی ری‌اکت همراه تسک های مفید برای یادگیری بهتر";
+        newData["teacher"] = "محسن اسفندیاری";
         newData["date"] = "25 اردیبهشت 1403";
-        newData["eye"] = (icons);
+        newData["eye"] = icons;
         return newData;
       });
       setCovertedData(i);
     }
   }, [isSuccess]);
 
+  const isMobile = window.innerWidth <= 768; // بررسی سایز صفحه
+
   return (
     <div>
-      <div className="bg-white w-full h-auto rounded-2xl mt-5">
-        <div className=" w-full h-70">
-          <Suspense fallback={<h1>loading...</h1>}>
-            {isSuccess && <BodyTableNews data={convertedData} />}
-          </Suspense>
+      {isMobile ? (
+        <ResponsivNews />
+      ) : (
+        <div className="bg-white w-full h-auto rounded-2xl mt-5">
+          <div className=" w-full h-70">
+            <Suspense fallback={<h1>loading...</h1>}>
+              {isSuccess && <BodyTableNews data={convertedData} />}
+            </Suspense>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
