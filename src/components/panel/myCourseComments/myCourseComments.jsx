32d7fcbb9bcb173ae0bbd874c2a1c805fcb492b7
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-17 12:11:22 +0330

    my comments

--- src/components/panel/myCourseComments/myCourseComments.jsx
+++ src/components/panel/myCourseComments/myCourseComments.jsx
@@ -1,19 +1,54 @@
-import React from "react";
-import FavBottomCourse from "../favCourse/favBottomCourse";
+// import React from "react";
+// import FavBottomCourse from "../favCourse/favBottomCourse";
+// import TableHolder from "./table/tableHolder";
+
+// const MyCourseComments = () => {
+//   return (
+//     <div>
+//       <div className="hidden sm:block">
+//         <h2 className="w-full h-10 mt-5 font-bold text-xl">کامنت های دوره</h2>
+//       </div>
+//       <div className="flex items-center">
+//         <div>
+//           <FavBottomCourse />
+//         </div>
+//       </div>
+//       <TableHolder />
+//     </div>
+//   );
+// };
+
+// export default MyCourseComments;
+
+import React, { useState } from "react";
 import TableHolder from "./table/tableHolder";
+import http from "./../../../core/services/interceptor";
+import { useQuery } from "@tanstack/react-query";
 
 const MyCourseComments = () => {
+  const [convertedData, setCovertedData] = useState([]);
+
+  const getMyCourseComment = async () => {
+    const res = await http.get(`/SharePanel/GetMyCoursesComments`);
+    return res;
+  };
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["myCourseComment"],
+    queryFn: getMyCourseComment,
+  });
+
   return (
     <div>
       <div className="hidden sm:block">
         <h2 className="w-full h-10 mt-5 font-bold text-xl">کامنت های دوره</h2>
       </div>
-      <div className="flex items-center">
-        <div>
-          <FavBottomCourse />
-        </div>
-      </div>
-      <TableHolder />
+      <TableHolder
+        data={data}
+        isSuccess={isSuccess}
+        convertedData={convertedData}
+        setCovertedData={setCovertedData}
+      />
     </div>
   );
 };
--- src/components/panel/myCourseComments/table/tableHolder.jsx
+++ src/components/panel/myCourseComments/table/tableHolder.jsx
@@ -1,10 +1,23 @@
-import React, { Suspense } from "react";
-import TableComp from "./tableComp";
-import FavBottomCourse from "../../favCourse/favBottomCourse";
+import React, { lazy, Suspense, useEffect } from "react";
 import { Spin } from "antd";
-import ResponsiveReserveMyCourse from "../../myReserveCourse/responsiveReserveMyCourse";
+const TableComp = lazy(() => import("./tableComp"));
 
-const TableHolder = () => {
+const TableHolder = ({ data, isSuccess, convertedData, setCovertedData }) => {
+  useEffect(() => {
+    if (isSuccess && data) {
+      const newData = data.myCommentsDtos.map((el) => {
+        return {
+          name: el.courseTitle,
+          title: el.title,
+          describe: el.describe,
+          accept: el.accept,
+          insertDate: el.insertDate,
+        };
+      });
+      setCovertedData(newData);
+    }
+  }, [isSuccess, data]);
+  console.log(data?.courseTitle);
   return (
     <div className=" h- ">
       <div className="bg-white w-full h- rounded-2xl mt-5">
@@ -16,11 +29,10 @@ const TableHolder = () => {
               </div>
             }
           >
-            <TableComp />
+            {isSuccess && <TableComp data={convertedData} />}
           </Suspense>
         </div>
       </div>
-      <ResponsiveReserveMyCourse />
     </div>
   );
 };
