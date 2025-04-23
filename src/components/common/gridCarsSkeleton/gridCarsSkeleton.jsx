5579465e994dec24dd7b66e5e27759524eb4a482
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-23 09:39:50 +0330

    skeleton fix

--- src/components/common/gridCarsSkeleton/gridCarsSkeleton.jsx
+++ src/components/common/gridCarsSkeleton/gridCarsSkeleton.jsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const GridCarsSkeleton = () => {
   return (
-    <div className="w-full my-5 rounded-2xl h-[300px] bg-lightGray ">
+    <div className="w-full my-5 rounded-2xl h-[300px] bg-lightGray border">
       <div className="flex 2xl:gap-5 gap-2 ">
         <div>
           <Skeleton.Image
             style={{ width: "350px", height: "300px", borderRadius: "16px" }}
           />
         </div>
-        <div className="mt-10 w-full mx-10">
-          <Skeleton />
-          <Skeleton.Input style={{ width: "650px", marginTop: "10px" }} />
-          <Skeleton.Input style={{ width: "650px", marginTop: "10px" }} />
+        <div className="mt-10 w-full mx -10">
+          <Skeleton style={{ width: "600px" }} />
+          <Skeleton.Input style={{ width: "600px", marginTop: "10px" }} />
+          <Skeleton.Input style={{ width: "600px", marginTop: "10px" }} />
         </div>
       </div>
     </div>
