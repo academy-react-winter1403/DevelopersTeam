c8c3bf762e2e20f7fabcb663420d3303f994a69b
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-04-24 21:35:12 +0330

    profile update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./slices/accountSlice";
+import profileReducer from "./slices/profileUpdateImgSlice";
 
 const store = configureStore({
   reducer: {
     accounts: accountReducer,
+    profile: profileReducer,
   },
 });
 
