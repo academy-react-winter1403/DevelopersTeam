e036cbacb942a3ec981a8a54b38015f91efc547c
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-05-23 22:22:06 +0330

    login

--- src/redux/ticketSlice/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ticketReducer from './ticketSlice'; // ایمپورت ریدوسر تیکت
-
-export const store = configureStore({
-  reducer: {
-    tickets: ticketReducer, // ثبت ریدوسر مربوط به تیکت‌ها
-  },
-});
