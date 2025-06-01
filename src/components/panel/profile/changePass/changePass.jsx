b8c0b93eb5f2e212d9851575cba82ba542417af8
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2025-06-02 01:52:39 +0330

    panel

--- src/components/panel/profile/changePass/changePass.jsx
+++ src/components/panel/profile/changePass/changePass.jsx
@@ -1,8 +1,10 @@
 import { Field, Form, Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import http from "./../../../../core/services/interceptor";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { IoEyeOffOutline } from "react-icons/io5";
 
 const ChangePass = () => {
   const queryClient = useQueryClient();
@@ -24,6 +26,23 @@ const ChangePass = () => {
     },
   });
 
+  const [showPass, setShowPass] = useState(false);
+  const handleShowPass = () => {
+    setShowPass(!showPass);
+  };
+  const show = (
+    <MdOutlineRemoveRedEye
+      onClick={handleShowPass}
+      className="absolute top-12 left-3"
+    />
+  );
+  const hide = (
+    <IoEyeOffOutline
+      onClick={handleShowPass}
+      className="absolute top-12 left-3"
+    />
+  );
+
   return (
     <div className="w-full px-4 md:px-0 md:w-xl h-96 ">
       <div className="flex flex-col space-y-3">
@@ -32,22 +51,25 @@ const ChangePass = () => {
           initialValues={{ oldPassword: "", newPassword: "" }}
         >
           <Form className=" space-y-4">
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 relative">
               <span className="relative">رمز قبلی</span>
               <Field
                 name="oldPassword"
+                type={showPass ? "text" : "password"}
                 className="w-full h-9 outline-none rounded-xl p-5 placeholder:text-xs border border-lightGray  bg-lightGray  focus:border-navyBlue transition-all duration-300"
                 placeholder="رمز قبلی خود را وارد کنید"
               />
+              {showPass ? hide : show}
             </div>
-            <div className="flex flex-col space-y-3">
+            <div className="flex flex-col space-y-3 relative">
               <span className="relative"> رمز جدید</span>
-
               <Field
                 name="newPassword"
+                type={showPass ? "text" : "password"}
                 className="w-full h-9 outline-none rounded-xl p-5 placeholder:text-xs border border-lightGray  bg-lightGray  focus:border-navyBlue transition-all duration-300"
                 placeholder="رمز جدید خود را وارد کنید"
               />
+              {showPass ? hide : show}
             </div>
             <button
               type="submit"
